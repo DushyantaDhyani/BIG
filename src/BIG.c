6c11411bf417546dc89197c445ed2d9cbc713b54
Dushyanta Dhyani <dush.dhyani@gmail.com>
2013-06-21 11:21:07 +0530

    Fixed Bugs in the previous version

--- src/BIG.c
+++ src/BIG.c
@@ -1,35 +1,51 @@
 #include<stdio.h>
 #include<malloc.h>
+#include<string.h>
+#include<stdlib.h>
 #include "../Include/BIG.h"
 #include "../Include/bool.h"
 BIG* newBIG(char *arr){
 	BIG *big=(BIG*)malloc(sizeof(BIG));
+	if(big==NULL){
+		printf("Memory Allocation Error for BIG\n");
+		exit(1);
+	}
 	int i=0;
 	int k=0;
+	int len=strlen(arr);
 	while(arr[i]=='0'){
 		i++;
 	}
-	if(arr[i]!='\0' && arr[i]=='-'){
-		big->sign=-1;
-		i++;
-	}
-	else{
-		while(arr[i]!='\0'){
-			big->data=(char *)malloc(sizeof(big->data));
-			big->data[k]=arr[i];	
-			k++;
+	if(arr[i]!='\0'){
+		if(arr[i]=='-'){
+			big->sign=-1;
 			i++;
 		}
-		if(k==0){
-			big->data[k++]='0';
+		else{
+			big->sign=1;
 		}
-		big->data[k]='\0';
-		big->length=k;
+		len=len-i;
 	}
+	//len=len-i;
+	big->length=len;	
+	big->data=(char *)malloc((len+1)*sizeof(char));
+	if(big->data=='\0'){
+		printf("Memory Allocation Error for Big's Data\n");
+		exit(1);
+	}
+	while(arr[i]!='\0'){
+		big->data[k]=arr[i];	
+		k++;
+		i++;
+	}
+	if(k==0){
+		big->data[k++]='0';
+	}
+	big->data[k]='\0';
 	return big;
 }
-char* toString(BIG *big){
-	char *str=(char *)malloc(big->size*sizeof(char));
+char* toStringBIG(BIG *big){
+	char *str=(char *)malloc(((big->length)+1)*sizeof(char));
 	int i=0;
 	while(big->data[i]!='\0'){
 		str[i]=big->data[i];
--- test.c
+++ test.c
@@ -1,8 +1,12 @@
 #include<stdio.h>
+#include<malloc.h>
 #include "Include/BIG.h"
 int main(){
 	char *str;
+	str=(char *)malloc(sizeof(str));
 	scanf("%s",str);
 	BIG *big=newBIG(str);
+	char *eqStr;
+	eqStr=toStringBIG(big);
 	return 0;
 }
